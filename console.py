8cf5c92c2cd2247325e6dd141a8cf7d2a201c4ed
Lilieyen <lnyasita@gmail.com>
2021-02-16 17:56:51 +0300

    console file

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This contains the console"""
+
+import models
+import cmd
+import shlex
+from datetime import datetime
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB command interpretor"""
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """if empty do nothing"""
+        return False
+
+    def do_quit(self, arg):
+        """exit program using quit command"""
+        return True
+
+    def do_EOF(self, arg):
+        """exit program using EOF"""
+        return True
